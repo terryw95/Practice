8867bce6876a7761bc40e6289b780458563b7fcb
terryw95 <93808574+terryw95@users.noreply.github.com>
2022-02-24 19:13:43 -0800

    Files for zodaicsupreme

---
+++ characters.js
@@ -0,0 +1,130 @@
+let buttonJacob = document.getElementById('button-jacob');
+let jacob = document.getElementById('jacob');
+let closeButtonJacob = document.getElementById('close-button-jacob');
+
+function openButtonJacob () {
+  buttonJacob.innerHTML = 'Jacob Torres';
+  jacob.style.display = 'block';
+  closeButtonJacob.style.display = 'block';
+}
+
+function closeItJacob () {
+  buttonJacob.innerHTML = 'Leo XY';
+  buttonJacob.style.cursor = 'pointer';
+  jacob.style.display = 'none';
+  closeButtonJacob.style.display = 'none';
+}
+
+buttonJacob.addEventListener('click', openButtonJacob);
+closeButtonJacob.addEventListener('click', closeItJacob);
+
+//Olivia
+
+let buttonOlivia = document.getElementById('button-olivia');
+let olivia = document.getElementById('olivia');
+let closeButtonOlivia = document.getElementById('close-button-olivia');
+
+function openButtonOlivia () {
+  buttonOlivia.innerHTML = 'Olivia King';
+  olivia.style.display = 'block';
+  closeButtonOlivia.style.display = 'block';
+}
+
+function closeItOlivia () {
+  buttonOlivia.innerHTML = 'Aquarius XX';
+  buttonOlivia.style.cursor = 'pointer';
+  olivia.style.display = 'none';
+  closeButtonOlivia.style.display = 'none';
+}
+
+buttonOlivia.addEventListener('click', openButtonOlivia);
+closeButtonOlivia.addEventListener('click', closeItOlivia);
+
+//Slade
+
+let buttonSlade = document.getElementById('button-slade');
+let slade = document.getElementById('slade');
+let closeButtonSlade = document.getElementById('close-button-slade');
+
+function openButtonSlade () {
+  buttonSlade.innerHTML = 'Slade Moretti';
+  slade.style.display = 'block';
+  closeButtonSlade.style.display = 'block';
+}
+
+function closeItSlade () {
+  buttonSlade.innerHTML = 'Scorpio XY';
+  buttonSlade.style.cursor = 'pointer';
+  slade.style.display = 'none';
+  closeButtonSlade.style.display = 'none';
+}
+
+buttonSlade.addEventListener('click', openButtonSlade);
+closeButtonSlade.addEventListener('click', closeItSlade);
+
+
+//Tim
+
+let buttonTim = document.getElementById('button-tim');
+let tim = document.getElementById('tim');
+let closeButtonTim = document.getElementById('close-button-tim');
+
+function openButtonTim () {
+  buttonTim.innerHTML = 'Timothy Watanabe';
+  tim.style.display = 'block';
+  closeButtonTim.style.display = 'block';
+}
+
+function closeItTim () {
+  buttonTim.innerHTML = 'Aquarius XY';
+  buttonTim.style.cursor = 'pointer';
+  tim.style.display = 'none';
+  closeButtonTim.style.display = 'none';
+}
+
+buttonTim.addEventListener('click', openButtonTim);
+closeButtonTim.addEventListener('click', closeItTim);
+
+//Patricia
+
+let buttonPatr = document.getElementById('button-patr');
+let patr = document.getElementById('patr');
+let closeButtonPatr = document.getElementById('close-button-patr');
+
+function openButtonPatr () {
+  buttonPatr.innerHTML = 'Patricia O\'Connor';
+  patr.style.display = 'block';
+  closeButtonPatr.style.display = 'block';
+}
+
+function closeItPatr () {
+  buttonPatr.innerHTML = 'Gemini XX';
+  buttonPatr.style.cursor = 'pointer';
+  patr.style.display = 'none';
+  closeButtonPatr.style.display = 'none';
+}
+
+buttonPatr.addEventListener('click', openButtonPatr);
+closeButtonPatr.addEventListener('click', closeItPatr);
+
+//Willow
+
+let buttonWill = document.getElementById('button-will');
+let will = document.getElementById('will');
+let closeButtonWill = document.getElementById('close-button-will');
+
+function openButtonWill () {
+  buttonWill.innerHTML = 'Willow Alexander';
+  will.style.display = 'block';
+  closeButtonWill.style.display = 'block';
+}
+
+function closeItWill () {
+  buttonWill.innerHTML = 'Pisces XX';
+  buttonWill.style.cursor = 'pointer';
+  will.style.display = 'none';
+  closeButtonWill.style.display = 'none';
+}
+
+buttonWill.addEventListener('click', openButtonWill);
+closeButtonWill.addEventListener('click', closeItWill);
